65960bda9a8d4e3db7b663e27600f1df069f7633
HelixFix <jerome.rinner@gmail.com>
2021-06-25 19:01:27 +0200

    :construction: Work in progress

--- Screen/Map.js
+++ Screen/Map.js
@@ -1,79 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Dimensions } from "react-native";
+import { Text, View, StyleSheet, Dimensions, Image, ToastAndroid } from "react-native";
 import * as Location from "expo-location";
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
 
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
-
-
-
-export default class Map extends React.Component{
-
-
+const latitudeDelta = 0.025;
+const longitudeDelta = 0.025;
+export default class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: null,
-      ErrorMsg: null,
-      region  : {
+      initialRegion  : {
         latitude      : 37.7507406,
         longitude     : 122.3417348,
         latitudeDelta : 0.0922,
-        longitudeDelta: 0.5 * (screenWidth / screenHeight),
+        longitudeDelta: 0.0122,
       },
     };
-    this.onRegionChange = this.onRegionChange.bind(this)
+
   }
 
-  onRegionChange(region) {
-    
-    this.setState({
-      region: {
-        latitude: this.state.location ? this.state.location.coords.latitude: 37,
-        longitude: this.state.location ? this.state.location.coords.longitude: 122,
-        latitudeDelta: 0.0022,
-        longitudeDelta: 0.0122,
-      }
-    })
-  }
-  
-  async useEffect() {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    console.log(status);
-    if (status !== "granted") {
-      this.setState({ ErrorMsg: "Permission to access location was denied" });
-      return;
-    }
+
+
+onChangeValue = initialRegion =>{
+  // alert(JSON.stringify(region))
+  ToastAndroid.show(JSON.stringify(initialRegion),ToastAndroid.SHORT)
+  this.setState({
+    initialRegion
+  })
+}
 
  
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location: location });
-    
-  }
-
 
   render() {
-    this.useEffect();
+   
 
     return (
-      
       <View style={styles.container}>
-         <MapView style={styles.map} 
-		 region={this.state.region}
-		 onRegionChange={this.onRegionChange}
-		>
-		
-		  <Marker
-			key        = {1}
-			coordinate = {{latitude: this.state.location ? this.state.location.coords.latitude : 47.7507406, longitude: this.state.location ? this.state.location.coords.longitude : 7.3417348}}
-			// title={marker.title}
-			title="Ma géoloc"
-			// description={marker.description}
-		  />
-	  </MapView>
-		 
+        <MapView style={styles.map}
+        initialRegion = {this.state.initialRegion}
+        onRegionChangeComplete = {this.onChangeValue}
+        />
+       
+          
+       
       </View>
     );
   }
@@ -81,17 +51,9 @@ export default class Map extends React.Component{
 
 const styles = StyleSheet.create({
   container: {
-    flex          : 1,
-    alignItems    : "center",
-    justifyContent: "center",
-    padding       : 20,
-  },
-  paragraph: {
-    fontSize : 18,
-    textAlign: "center",
+    flex: 1,
   },
   map: {
-    width : Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    flex: 1,
   },
 });
